e034f9ef873ef7684e168438cda7cb90ac8362b8
gtesei <gtesei@yahoo.com>
2013-08-30 04:43:22 +0000

    updated web.js reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require("fs");
+
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var buff = new Buffer(100);
+
+ app.get('/', function(request, response) {
+  fs.readFileSync("index.html", function (err,data) {
+    if (err) throw err;
+    buff.write(data);
+
+  });
+
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
